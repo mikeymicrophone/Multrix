da71dfe718a6dbf43c0b3b31bf8526bf5207b946
Mike Schwab <mike.schwab@gmail.com>
2026-01-13 19:21:33 -0500

    Initial Commit

---
+++ Multrix/MultrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultrixApp.swift
+//  Multrix
+//
+//  Created by Mike Schwab on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct MultrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
